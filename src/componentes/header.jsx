e3533896137ed6457c3296d36b2eafd4ab8d88cf
Emarroquin1 <Erickmarroquin503@hotmail.com>
2022-12-27 10:58:07 -0600

    Update header.jsx

--- src/componentes/header.jsx
+++ src/componentes/header.jsx
@@ -8,14 +8,14 @@ return(
 
     <nav className="navbar navbar-expand-lg navbar-dark  bg-blue">
   <div className="container-fluid">
-    <a className="navbar-brand" href="/">Salazar Services</a>
+    <a className="navbar-brand" href="/ReactSalazarService">Salazar Services</a>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
     <div className="collapse navbar-collapse justify-content-center" id="navbarNavDropdown">
       <ul className="navbar-nav justify-content-center">
         <li className="nav-item">
-          <a className="nav-link active" aria-current="page" href="/"  >Home</a>
+          <a className="nav-link active" aria-current="page" href="/ReactSalazarService"  >Home</a>
         </li>
         <li className="nav-item">
           <a className="nav-link"  href="/contact" >Contact</a>
